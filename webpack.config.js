2ca2638836023519139ccbfa94e1855a980e0b33
zhousg <zhoushugang@itcast.cn>
2017-12-25 23:59:20 +0800

    :fire:

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: './vueComponent/dist/',
+    publicPath: './dist/',
     filename: 'build.js'
   },
   module: {
